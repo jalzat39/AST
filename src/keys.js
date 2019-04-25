88e5358283b59837e9fb736c9dc8aba666a2a99a
root <root@ip-172-31-82-75.ec2.internal>
2019-04-25 05:58:35 +0000

    3

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb: {
-      URI: 'mongodb://mongo-server/login-AST'
+      URI: 'mongodb://mongo-server/login-ast'
       //URI: 'mongodb://localhost:27017/login-AST'
   }
 };
