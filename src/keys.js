ba7e06d7a2258feab414eea7acd6f3a36602fb1b
jalzat39 <jmiguelalzate@gmail.com>
2019-04-26 21:00:16 -0500

    problemas

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,6 @@
 module.exports = {
   mongodb: {
-<<<<<<< HEAD
       //URI: 'mongodb://mongo-server/login-AST'
       URI: 'mongodb://localhost:27017/login-AST'
-=======
-      URI: 'mongodb://mongo-server/login-ast'
-      //URI: 'mongodb://localhost:27017/login-AST'
->>>>>>> 9b8235645344a5416022bda0b5043c91b99dc119
   }
 };
