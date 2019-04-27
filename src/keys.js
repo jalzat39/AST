4aac5a5023e47eb23eacd7b47531d4cb3160330e
jalzat39 <jmiguelalzate@gmail.com>
2019-04-26 20:57:29 -0500

    intento

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb: {
-      URI: 'mongodb://mongo-server/login-AST'
-      //URI: 'mongodb://localhost:27017/login-AST'
+      //URI: 'mongodb://mongo-server/login-AST'
+      URI: 'mongodb://localhost:27017/login-AST'
   }
 };
