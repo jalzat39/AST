3df9b090d08810a4755424cc9d519926014ad7fd
jalzat39 <jmiguelalzate@gmail.com>
2019-05-13 13:12:45 -0500

    intento atlas

--- src/database.js
+++ src/database.js
@@ -1,18 +1,29 @@
-//const mongoose = require('mongoose');
-//const { mongodb } = require('./keys');
+const mongoose = require('mongoose');
+const { mongodb } = require('./keys');
 
-//mongoose.set('useFindAndModify', false);
-//mongoose.connect(mongodb.URI, {
-  //useNewUrlParser: true
-//})
-  //.then(db => console.log('DB is connected'))
-  //.catch(err => console.log(err));
+mongoose.set('useFindAndModify', false);
+mongoose.connect(mongodb.URI, {
+  useNewUrlParser: true
+})
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.log(err));
+  
+/*const MongoClient = require('mongodb').MongoClient;
 
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = "mongodb+srv://admin:2j1JHQFU1Od729N5@cluster0-youmh.mongodb.net/test?retryWrites=true";
-  const client = new MongoClient(uri, { useNewUrlParser: true });
-  client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://admin:84f5EG0fILLCIydq@cluster0-youmh.mongodb.net/test?retryWrites=true"
+MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
+  if(err) {
+    console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+  }
+    else {
+      console.log('Connected...');
+      const db = client.db("test").collection("devices", function (err, collection) {
+        collection.find().toArray(function(err, res) {
+                     callback(res)});
+        })
+   // perform actions on the collection object
     client.close();
-  });
+    }
+});
+*/ 
