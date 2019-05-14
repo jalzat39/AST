20b60df1e8a741d8375322bd9673872046f74733
jalzat39 <jmiguelalzate@gmail.com>
2019-05-14 03:37:16 -0500

    correccion errores del merge

--- src/database.js
+++ src/database.js
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-=======
-/*const mongoose = require('mongoose');
-const { mongodb} = require('./keys');
-
-mongoose.set('useFindAndModify', false);
-mongoose.connect(mongodb.URI, {
-  useNewUrlParser: true
-})
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.log(err));*/
-  
->>>>>>> release
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
 
 mongoose.connect("mongodb+srv://mono:mono123@cluster0-qfesm.mongodb.net/test?retryWrites=true", { useNewUrlParser: true })
-      .then(db => console.log('db connected')).catch(err => console.log(err));
+.then(db => console.log('db connected')).catch(err => console.log(err));
